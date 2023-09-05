f73df0e1084d4539457c8475db5012e6170ed34b
CaseyKEOS <caseykentemmanuel@gmail.com>
2023-09-05 22:39:51 +0800

    Add project files.

---
+++ database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace weekthree
+{
+    public class database
+    {
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\ProjectModels;Initial Catalog=week3;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+
+        public void save(string fullname, string email, string phone, string message)
+        {
+            con.Open();
+                SqlCommand cmd = new SqlCommand("insert into tblweb values ('" + fullname + "','" + email + "','" + phone + "','" + message + "')", con);
+                cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
---
+++ view/contact.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace weekthree.view
+{
+    public partial class contact : System.Web.UI.Page
+    {
+        database db = new database();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            db.save(name.Text, email.Text, phone.Text, message.Text);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Record added')", true);
+        }
+    }
+}
